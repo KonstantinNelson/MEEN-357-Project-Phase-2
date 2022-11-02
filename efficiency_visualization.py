a84232eb4648469ec4c94f633597adb83005f202
EvanWarrick <114360240+EvanWarrick@users.noreply.github.com>
2022-11-01 20:56:56 -0500

    Update efficiency_visualization.py

--- efficiency_visualization.py
+++ efficiency_visualization.py
@@ -3,14 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 from rover_dict import *
 
+#get the effcy array from the rover dictionary
 effcy = rover['wheel_assembly']['motor']['effcy']
+#get the effcy_tau array from teh rover dict
 effcy_tau = rover['wheel_assembly']['motor']['effcy_tau']
 
+#define a function effcy_fun that interpolates the values of effcy_tau and effcy
 effcy_fun = inte.interp1d(effcy_tau,effcy,kind='cubic')
 
+#create an array of 100 values ranging from the minimum torque value to the maximum torque value
 tau = np.linspace(effcy_tau[0],effcy_tau[-1],100)
+#approximate the intermediate values of eff using the interpolation function and tau
 eff = effcy_fun(tau)
 
+#graph the values from the effcy_tau and affcy dicts, and the values of eff from effcy_fun
 plt.plot(tau,eff)
 plt.scatter(effcy_tau,effcy,marker='*')
 plt.xlabel('Torque (N*m)')
